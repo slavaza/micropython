8bb79a6d44ff912fa1049330254d09a036c419fb
Vyacheslav Azarov <37409681+slavaza@users.noreply.github.com>
2020-10-15 19:40:22 +0300

    Update uart_core.c

--- ports/avr8-dummy/uart_core.c
+++ ports/avr8-dummy/uart_core.c
@@ -1,13 +1,9 @@
 #include <unistd.h>
 #include "py/mpconfig.h"
 
-// Receive single character dummy
-int mp_hal_stdin_rx_chr(void) {
-    return 0;
-}
-
 // Send string of given length dummy
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
 (void)str;
 (void)len;
 }
+
